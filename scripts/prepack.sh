d2f9ca6dcce8a52b7f6519ab0f0a010403497ff9
张大漾 <1417376255@qq.com>
2019-08-10 01:19:51 +0800

    update scripts

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -2,13 +2,26 @@
 
 set -e # 遇到错误时停止执行
 
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux/5947802#5947802
+RED="\033[0;31m"
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+RESET="\033[0m"
+
 base=$( cd "$(dirname "$0")" ; pwd -P )/../
 
-echo 'build'
+# echo "${GREEN}> git status...${RESET}"
+GIT_NOT_CLEAN=$(git status -s)
+if [ -n "$GIT_NOT_CLEAN" ]; then
+    echo "${RED}> Please git commit you changes${RESET}"
+    echo "$GIT_NOT_CLEAN\n"
+    exit 1
+else
+  echo "successs"
+fi
 
-git commit -am "publish"
-npm run build
 npm version patch
+npm run build
 
 echo 'prepare publish files'
 cd $base/dist/
@@ -16,3 +29,4 @@ for file in *; do
   echo 'prepare '$base$file
   cp -R $file $base$file
 done
+
