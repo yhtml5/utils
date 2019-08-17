0cf9dcc709c98f9454275a7339e74047b779014f
张大漾 <1417376255@qq.com>
2019-08-17 17:25:30 +0800

    feat: formatNumber option

--- src/formatNumber.test.js
+++ src/formatNumber.test.js
@@ -1,35 +1,27 @@
-'use strict';
+'use strict'
 const expect = require('chai').expect
 const formatNumber = require('../dist/formatNumber').default
 
-console.log(formatNumber)
-
 describe('test function formatNumber', () => {
-  const value = 999999.715001
-  const a = formatNumber({
+  const num = 999999.715001
+
+  const a = formatNumber(num)
+
+  const b = formatNumber(num, {
+    decimals: 3,
+    decPoint: '.',
+    roundtag: 'ceil'
+  })
+
+  const c = formatNumber(num, {
     decimals: 2,
     decPoint: '.',
     thousandsSep: ',',
     roundtag: 'round'
   })
-  const b = formatNumber({
-    decimals: 3,
-    decPoint: '.',
-    roundtag: 'ceil'
-  })
-  const c = formatNumber()
 
-  it('test a', () => {
-    const result = a(value)
-    expect(result).to.equal('999,999.72');
-  })
-  it('test b', () => {
-    const result = b(value)
-    expect(result).to.equal('999999.716');
-  })
-  it('test c', () => {
-    const result = c(value)
-    expect(result).to.equal('1000000');
-  })
+  it('expect: 1000000', () => expect(a).to.equal('1000000'))
+  it('expect: 999999.716', () => expect(b).to.equal('999999.716'))
+  it('expect: 999,999.72', () => expect(c).to.equal('999,999.72'))
 
 })
--- src/formatNumber.ts
+++ src/formatNumber.ts
@@ -1,5 +1,4 @@
 interface Config {
-  number?: number;        //  要格式化的数字
   decimals?: number;      // 保留几位小数, 默认 0
   util?: number;          // 多少位开启换单位, 默认 万
   roundtag?: string;      // 舍入参数, [ceil: 向上取整, floor: 向下取整, round: 四舍五入], 默认 round
@@ -7,8 +6,8 @@ interface Config {
   thousandsSep?: string;  // 千分位符号, 默认 无
 }
 
-function formatNumber(values: Config) {
-  const { decimals = 0, util = 4, decPoint = '.', thousandsSep = '', roundtag = 'round', } = values || {}
+function formatNumber(number: number | string, option: Config) {
+  const { decimals = 0, util = 4, decPoint = '.', thousandsSep = '', roundtag = 'round', } = option || {}
 
   // if (process.env.NODE_ENV !== 'production') {
   if (Object.prototype.toString.call(decimals) !== '[object Number]') {
@@ -18,34 +17,30 @@ function formatNumber(values: Config) {
   if (['ceil', 'floor', 'round'].indexOf(roundtag) < 0) {
     throw `formatNumber\'s parameter roundtag can only be one of the [ceil,floor,round] or be Omitted, but get ${roundtag}`
   }
-  // }
 
-  return (number: string) => {
-    // let number = JSON.parse(JSON.stringify(value))
-    number = (number + '').replace(/[^0-9+-Ee.]/g, '')
-    const n = !isFinite(+number) ? 0 : +number
-    const prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
-    let s: string | string[] = ''
+  number = (number + '').replace(/[^0-9+-Ee.]/g, '')
+  const n = !isFinite(+number) ? 0 : +number
+  const prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
+  let s: string | string[] = ''
 
-    function toFixedFix(n: number, prec: number) {
-      const k = Math.pow(10, prec);
-      console.log();
-      // @ts-ignore
-      return '' + parseFloat(Math[roundtag](parseFloat((n * k).toFixed(prec * 2))).toFixed(prec * 2)) / k;
-    };
-    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-    if (thousandsSep) {
-      const re = /(-?\d+)(\d{3})/;
-      while (re.test(s[0])) {
-        s[0] = s[0].replace(re, "$1" + thousandsSep + "$2");
-      }
+  function toFixedFix(n: number, prec: number) {
+    const k = Math.pow(10, prec);
+    console.log();
+    // @ts-ignore
+    return '' + parseFloat(Math[roundtag](parseFloat((n * k).toFixed(prec * 2))).toFixed(prec * 2)) / k;
+  };
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (thousandsSep) {
+    const re = /(-?\d+)(\d{3})/;
+    while (re.test(s[0])) {
+      s[0] = s[0].replace(re, "$1" + thousandsSep + "$2");
     }
-    if ((s[1] || '').length < prec) {
-      s[1] = s[1] || '';
-      s[1] += new Array(prec - s[1].length + 1).join('0');
-    }
-    return s.join(decPoint);
   }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(decPoint);
 }
 
 export default formatNumber
