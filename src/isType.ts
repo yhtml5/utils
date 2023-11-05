5c9c6475601d160a9e7a8a80e4339e4d7a7f9642
张大漾 <1417376255@qq.com>
2023-11-05 20:59:15 +0800

    update: type judgment

--- src/isType.ts
+++ src/isType.ts
@@ -3,12 +3,15 @@
 *  types: ["Array", "Boolean", "Date", "Number", "Object", "RegExp", "String", "Window", "HTMLDocument"]
 */
 
-export const isUndefined = (value: any) => typeof value === 'undefined'
-export const isNull = (value: any) => Object.prototype.toString.call(value) === '[object Null]'
-export const isNumber = (value: any) => Object.prototype.toString.call(value) === '[object Number]'
-export const isString = (value: any) => Object.prototype.toString.call(value) === '[object String]'
-export const isFunction = (value: any) => Object.prototype.toString.call(value) === '[object Function]'
-export const isArray = (value: any) => Object.prototype.toString.call(value) === '[object Array]'
-export const isArrayEmpty = (value: any) => isArray(value) && value.length === 0
-export const isObject = (value: any) => Object.prototype.toString.call(value) === '[object Object]'
-export const isObjectEmpty = (value: any) => isObject(value) && Object.keys(value).length === 0
+export const isUndefined = (value: any): boolean => typeof value === 'undefined'
+export const isNull = (value: any): boolean => Object.prototype.toString.call(value) === '[object Null]'
+export const isNumber = (value: any): boolean => Object.prototype.toString.call(value) === '[object Number]'
+export const isString = (value: any): boolean => Object.prototype.toString.call(value) === '[object String]'
+export const isSymbol = (value: any): boolean => Object.prototype.toString.call(value) === '[object Symbol]'
+
+export const isFunction = (value: any): boolean => Object.prototype.toString.call(value) === '[object Function]'
+export const isArray = (value: any): boolean => Object.prototype.toString.call(value) === '[object Array]'
+export const isArrayEmpty = (value: any): boolean => isArray(value) && value.length === 0
+export const isArrayNotEmpty = (value: any): boolean => isArray(value) && value.length > 0
+export const isObject = (value: any): boolean => Object.prototype.toString.call(value) === '[object Object]'
+export const isObjectEmpty = (value: any): boolean => isObject(value) && Object.keys(value).length === 0
